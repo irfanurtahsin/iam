34831e5485bff3e03d4e2bec84cdc2cc5e9a92e4
irfanurtahsin <irfanurtahsin@gmail.com>
2025-01-21 20:39:20 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-container'),
+    renderer: 'svg', // Options: 'svg', 'canvas', 'html'
+    loop: true,  // Animation loops infinitely
+    autoplay: true,  // Starts playing immediately
+    path: 'https://lottie.host/cf82d2df-8d05-4f77-a982-8b3d0aa7728c/aChGAPkiWp.lottie' // Replace with your Lottie animation URL or local path
+});
