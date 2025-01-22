200acf1664d097239caa494d9bce131c9ad4f12d
irfanurtahsin <irfanurtahsin@gmail.com>
2025-01-22 15:28:49 +0300

    added about section

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var animation = lottie.loadAnimation({
-    container: document.getElementById('lottie-container'),
-    renderer: 'svg', // Options: 'svg', 'canvas', 'html'
-    loop: true,  // Animation loops infinitely
-    autoplay: true,  // Starts playing immediately
-    path: 'https://lottie.host/cf82d2df-8d05-4f77-a982-8b3d0aa7728c/aChGAPkiWp.lottie' // Replace with your Lottie animation URL or local path
+document.addEventListener("DOMContentLoaded", function() {
+    // Load Call Animation
+    lottie.loadAnimation({
+        container: document.getElementById('call-animation'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'https://lottie.host/cf82d2df-8d05-4f77-a982-8b3d0aa7728c/aChGAPkiWp.lottie'
+    });
+
+    // Load WhatsApp Animation
+    lottie.loadAnimation({
+        container: document.getElementById('whatsapp-animation'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'https://lottie.host/58cd9742-68af-4f3a-8654-7c811b43885d/1L88jTPoST.lottie'
+    });
 });
